f5ed2442ce5e18651665ba5336c36480db8f3fc6
Evan Flynn <erflynn21@gmail.com>
2022-11-03 13:42:47 +0800

    adds cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
 import path from "path";
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
+    prerender: {
+      enabled: true
+    },
   },
 
   preprocess: [

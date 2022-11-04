47bad7f31353bb4f6d428c7faef5e0994262b209
Evan Flynn <erflynn21@gmail.com>
2022-11-04 12:54:55 +0800

    changes code in middleware function

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,27 +1,46 @@
-import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+// import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-export const onRequest = mailchannelsPlugin({
+// export const onRequest = mailchannelsPlugin({
 
+//   personalizations: [
+//     {
+//       to: [{ name: "Evan", email: "evan@evanflynn.com" }],
+//     },
+//   ],
+//   from: (params) => {
+//       return {
+//           name: params.formData.get("name"),
+//           email: params.formData.get("email"),
+//       }
+//   },
+//   subject: 'New contact form submission from portfolio site',
+//   content: (params) => {
+//       return {
+//           text: params.formData.get("message"),
+//       }
+//   },
+//   respondWith: () => {
+//     return new Response(
+//       `Thank you for getting in touch! I will respond as soon as possible.`
+//     );
+//   },
+// });
+
+import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+
+export const onRequest: PagesFunction = mailChannelsPlugin({
   personalizations: [
     {
-      to: [{ name: "Evan", email: "evan@evanflynn.com" }],
+      to: [{ name: "ACME Support", email: "support@example.com" }],
     },
   ],
-  from: (params) => {
-      return {
-          name: params.formData.get("name"),
-          email: params.formData.get("email"),
-      }
-  },
-  subject: 'New contact form submission from portfolio site',
-  content: (params) => {
-      return {
-          text: params.formData.get("message"),
-      }
+  from: {
+    name: "ACME Support",
+    email: "support@example.com",
   },
   respondWith: () => {
     return new Response(
-      `Thank you for getting in touch! I will respond as soon as possible.`
+      `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
     );
   },
 });

323736e28af0c841ce0dbbcf3ed00cf89eea48cf
Evan Flynn <erflynn21@gmail.com>
2022-11-04 13:13:16 +0800

    changes middleware function

---
+++ functions/_middleware.js
@@ -0,0 +1,45 @@
+import mailchannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+
+export const onRequest = mailchannelsPlugin({
+  personalizations: [
+    {
+      to: [{ name: "Evan", email: "evan@evanflynn.com" }],
+    },
+  ],
+  from: (params) => {
+      return {
+          name: params.formData.get("name"),
+          email: params.formData.get("email"),
+      }
+  },
+  subject: 'New contact form submission from portfolio site',
+  content: (params) => {
+      return {
+          text: params.formData.get("message"),
+      }
+  },
+  respondWith: () => {
+    return new Response(
+      `Thank you for getting in touch! I will respond as soon as possible.`
+    );
+  },
+});
+
+// import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+
+// export const onRequest: PagesFunction = mailChannelsPlugin({
+//   personalizations: [
+//     {
+//       to: [{ name: "Evan Flynn", email: "evan@evanflynn.com" }],
+//     },
+//   ],
+//   from: {
+//     name: "ACME Support",
+//     email: "support@example.com",
+//   },
+//   respondWith: () => {
+//     return new Response(
+//       `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
+//     );
+//   },
+// });

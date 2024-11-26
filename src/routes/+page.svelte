4117cceda9115b540c5396d64774e2ec6186b885
Evan Flynn <erflynn21@gmail.com>
2024-11-26 13:57:54 -0500

    fixing typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,8 +15,8 @@
   <h2 class="text-4xl mb-8">hi, I'm Evan</h2>
 
   <h1 class="text-6xl font-medium mb-8 text-primary">
-    I make greater user experiences through technical support and user-friendly/
-    performant websites, web apps, and mobile apps.
+    I make greater user experiences through technical support and
+    user-friendly/performant websites, web apps, and mobile apps.
   </h1>
 
   <p class="text-2xl mb-4 leading-10">
